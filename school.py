da59fdefac8de882e788bd59df6c793e39beebb7
Methasha20250805@iit.ac.lk <methasha.20250805@iit.ac.lk>
2026-08-09 19:44:02 +0530

    initial commit

---
+++ school.py
@@ -0,0 +1,14 @@
+"""
+Simple School Management System
+--------------------------------
+Lets you:
+  - Add students
+  - Add teachers
+  - Create classes (subjects)
+  - Assign a teacher to a class
+  - Enroll a student in a class
+  - View everything currently stored
+
+All data lives in memory (Python dictionaries/lists) while the
+program is running. Data resets each time you restart the program.
+"""
