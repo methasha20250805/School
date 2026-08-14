3cb6df91075988cd156e6f667a1bd39cd454f725
Methasha20250805@iit.ac.lk <methasha.20250805@iit.ac.lk>
2026-08-14 19:47:01 +0530

    comments

--- school.py
+++ school.py
@@ -7,8 +7,6 @@
 #  Enroll a student in a class
 #  View everything currently stored
 
-#All data lives in memory (Python dictionaries/lists) while the program is running. Data resets each time you restart the program.
-
 class Person:
     def __init__(self, person_id, name):
         self.id = person_id
