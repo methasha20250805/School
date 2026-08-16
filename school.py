39dad9dce63859e3f1427ffa0bfbe3126f43c01d
Methasha20250805@iit.ac.lk <methasha.20250805@iit.ac.lk>
2026-08-16 18:15:23 +0530

    editing

--- school.py
+++ school.py
@@ -131,10 +131,10 @@ class School:
 
 MENU = """
 School Management System
-  1. Add student
-  2. Add teacher
-  3. Add class
-  4. Assign teacher to class
+    1. Add student
+    2. Add teacher
+    3. Add class
+    4. Assign teacher to class
   5. Enroll student in class
   6. View students
   7. View teachers
